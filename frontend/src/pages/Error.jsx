b771bf0db7ddcdb3ab2bdc4e3bdc385fd5bacfaa
orunto <oruntoeniola@aol.com>
2023-10-20 19:23:34 +0100

    fix: go back link

--- frontend/src/pages/Error.jsx
+++ frontend/src/pages/Error.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import { useNavigate } from "react-router-dom";
 
 export default function Error () {
-    let history = useNavigate();
+    let navigate = useNavigate();
     return (
         <>
             <Helmet>
@@ -20,7 +20,7 @@ export default function Error () {
                     </div>
                 </form>
 
-                <p><Link onClick={() => history.goBack()}>Go Back <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"><path d="M9 14L5 10L9 6" stroke="#32D583" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/><path d="M5 10H16C17.0609 10 18.0783 10.4214 18.8284 11.1716C19.5786 11.9217 20 12.9391 20 14C20 15.0609 19.5786 16.0783 18.8284 16.8284C18.0783 17.5786 17.0609 18 16 18H15" stroke="#32D583" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></Link></p>                
+                <p><Link onClick={() => navigate(-1)}>Go Back <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"><path d="M9 14L5 10L9 6" stroke="#32D583" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/><path d="M5 10H16C17.0609 10 18.0783 10.4214 18.8284 11.1716C19.5786 11.9217 20 12.9391 20 14C20 15.0609 19.5786 16.0783 18.8284 16.8284C18.0783 17.5786 17.0609 18 16 18H15" stroke="#32D583" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></Link></p>                
             </main>
         
         </>
