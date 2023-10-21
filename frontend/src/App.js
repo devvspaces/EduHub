dde74079aa21c2dd0b007077cbc6a0d438d4607b
Orunto Eniola <98480650+orunto@users.noreply.github.com>
2023-10-21 10:25:32 +0100

    Feat/login page (#15)
    * sign up page with stub pending sign up api
    
    * node_modules removed
    
    * notiflix install error
    
    * notiflix install error corrected
    
    * login page
    
    * semantics
    
    * semantics
    
    * favicon
    
    * semantics and background image
    
    * fix: background on phone screens
    
    * error page
    
    * fix: go back link

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import { Route, Routes } from 'react-router-dom';
 
 import './App.scss';
 
-import { SignUp } from './pages';
+import { SignUp, SignIn, Error } from './pages';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<SignUp/>} />
+      <Route path='/login' element={<SignIn/>} />
+      <Route path='*' element={<Error/>} />
     </Routes>    
   );
 }
