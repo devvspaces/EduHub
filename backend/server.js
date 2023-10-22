ab061a755ed7990cd902e7ed82e2cc2606e7014f
Evidence Adejuwon <adejuwonevidence181@gmail.com>
2023-10-22 13:38:29 +0100

    Create User function  (#16)
    * ignore environment variables
    
    * env format
    
    * update env format
    
    * user schema model
    
    * installed dependencies
    
    * added cors and json parser
    
    * installed uuid package
    
    * added uuid for id
    
    * added bcrypt
    
    * create user function
    
    * user.js file renamed

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 const connectToDatabase = require('./utils/db.js');
+const cors = require('cors');
 
 const db = connectToDatabase();
+
+//  Middlewares
+app.use(cors());
+app.use(express.json());
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
