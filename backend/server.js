ca64480401e4cfaf935e4b6ed8c2830a6c4b858c
CybertronicEvidence <adejuwonevidence181@gmail.com>
2023-10-24 23:41:40 +0100

    added APIs to server path

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const app = express()
 const port = 3000
 const connectToDatabase = require('./utils/db.js');
 const cors = require('cors');
+const apiRouter = require("./routes/routes.js");
 
 const db = connectToDatabase();
 
@@ -12,6 +13,7 @@ const db = connectToDatabase();
 app.use(cors());
 app.use(express.json());
 
+app.use('/api', apiRouter)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
