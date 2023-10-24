f939e06713ef99da9b140fb26cecb229f0579f45
CybertronicEvidence <adejuwonevidence181@gmail.com>
2023-10-24 14:00:57 +0100

    delete user function

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -41,8 +41,20 @@ const forgotPassword = async (req, res) => {
     }
 };
 
+const deleteUser = async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user) res.status(404).json({ message: 'User does not exist' });
+        await user.deleteOne();
+        res.status(200).json({ message: 'User deleted...' });
+    } catch (err) {
+        res.status(500).json(err?.message || 'An Error Occured!');
+    }
+};
+
 module.exports = {
     getUser,
     updateUser,
-    forgotPassword
+    forgotPassword,
+    deleteUser
 }
