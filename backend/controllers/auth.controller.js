42cd23e7a35846da9a17673140e8d29aa0eb5e16
CybertronicEvidence <adejuwonevidence181@gmail.com>
2023-10-24 13:21:57 +0100

    moved auth functions to auth controller

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,31 @@
+const User = require('../models/user.model');
+const bcrypt = require('bcrypt');
+
+const createUser = async (req, res) => {
+    try {
+        const { user_id, email, password, role } = req.body;
+
+        const salt = await bcrypt.genSalt();
+        const hash = await bcrypt.hash(password, salt);
+
+        const user = new User({
+            user_id,
+            email,
+            password: hash,
+            role
+        })
+
+        await user.save();
+        res.status(200).json({ status: 201, data: user })
+    } catch (err) {
+        res.status(500).json(err?.message)
+    }
+}
+
+const loginUser = async (req, res) => {
+    try {
+        const { } = req.body;
+    } catch (err) {
+        res.status(500).json(err?.message)
+    }
+}
