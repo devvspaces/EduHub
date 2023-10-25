97bef8c3e6d1c2233c134bb68b09838fda7c1d01
CybertronicEvidence <adejuwonevidence181@gmail.com>
2023-10-25 01:13:23 +0100

    modified db config

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -4,18 +4,18 @@ const mongoose = require('mongoose');
 const DB_URL = process.env.MONGODB_URL;
 
 async function connectToDatabase() {
-    try {
-        
-      // Connect to the MongoDB database using Mongoose.
-      await mongoose.connect(DB_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
-  
-      console.log('Connected to MongoDB');
-    } catch (error) {
-      console.error('MongoDB connection error:', error);
-    }
+  try {
+
+    // Connect to the MongoDB database using Mongoose.
+    await mongoose.connect(DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
   }
-  
-  module.exports = connectToDatabase;
+}
+
+module.exports = connectToDatabase;
